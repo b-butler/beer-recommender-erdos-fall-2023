9a008ddc1a1fd3fc6c6cc4c4ac2e39acec5935d6
Brandon Butler <butlerbr@umich.edu>
2023-11-21 21:21:36 -0500

    refactor: Simplify validation logic

--- train_test_split.py
+++ train_test_split.py
@@ -19,39 +19,39 @@ def get_splits(int_matrix, random_state=42):
     indices = int_matrix.indices
     indptr = int_matrix.indptr
 
-    users = [0]*int_matrix.shape[0]
+    N_users = int_matrix.shape[0]
     test_sets = [[] for _ in range(n_splits)]
     train_set = [] # this set is never in the test set
     # for each user, separate their reviews into a train set and different test sets.
-    for i in range(len(users)):
+    for i in range(N_users):
         row_start = indptr[i]
         row_end = indptr[i + 1]
-        users[i] = indices[row_start:row_end] # these are the beers that user i reviewed
+        user_reviews = indices[row_start:row_end] # these are the beers that user i reviewed
+        n_reviews = len(user_reviews)
         # place the users in buckets based on number of reviews
-        if len(users[i]) < 5:
-            for beer_idx in users[i]:
+        if n_reviews < 5:
+            for beer_idx in user_reviews:
                 train_set.append([i,beer_idx])
-        elif len(users[i]) < 11:
-            users[i] = np.random.permutation(users[i])
+        elif n_reviews < 11:
+            user_reviews = np.random.permutation(user_reviews)
             for j in range(n_splits):
-                test_sets[j].append([i,users[i][j]])
-            for beer_idx in users[i][n_splits:]:
+                test_sets[j].append([i,user_reviews[j]])
+            for beer_idx in user_reviews[n_splits:]:
                 train_set.append([i,beer_idx])
         else :
-            n = len(users[i])
-            k = n//10
-            users[i] = np.random.permutation(users[i])
+            k = n_reviews // 10
+            user_reviews = np.random.permutation(user_reviews)
             for j in range(n_splits):
-                for beer_idx in users[i][j*k:(j+1)*k]:
+                for beer_idx in user_reviews[j*k:(j+1)*k]:
                     test_sets[j].append([i, beer_idx])
-            for beer_idx in users[i][n_splits*k:]:
+            for beer_idx in user_reviews[n_splits*k:]:
                 train_set.append([i, beer_idx])
     # create the splits by appending all but one test set to the train set
-    splits = [0]*n_splits
+    splits = []
     for j in range(n_splits):
         train_j = train_set.copy()
         for i in range(n_splits):
             if i != j:
                 train_j.extend(test_sets[i])
-        splits[j] = (np.array(train_j), np.array(test_sets[j]))
+        splits.append((np.array(train_j), np.array(test_sets[j])))
     return splits
