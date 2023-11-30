909ebf8530fbb740b2e648c93f09e443d10541b7
Brandon Butler <butlerbr@umich.edu>
2023-11-30 11:40:27 -0500

    misc: Format all code with ruff format

--- clean.py
+++ clean.py
@@ -1,4 +1,6 @@
 """Functions for cleaning the Beer Advocate data before processing."""
+
+
 def remove_null_rows(data, columns=("review_profilename", "review_overall")):
     """Remove rows with a null in given columns.
 
@@ -36,16 +38,24 @@ def get_beers(data):
     data: pandas.DataFrame
         The Beer Advocate data frame.
     """
-    beers = data.copy().drop(['review_time',
-                              'review_overall',
-                              'review_aroma',
-                              'review_appearance',
-                              'review_profilename',
-                              'review_palate',
-                              'review_taste'], axis = 1) # drop all columns having to do with a specific review
-    beers = beers.drop_duplicates() # and then eliminate EXACT duplicates just keeping the first instance of each
+    beers = data.copy().drop(
+        [
+            "review_time",
+            "review_overall",
+            "review_aroma",
+            "review_appearance",
+            "review_profilename",
+            "review_palate",
+            "review_taste",
+        ],
+        axis=1,
+    )  # drop all columns having to do with a specific review
+    beers = (
+        beers.drop_duplicates()
+    )  # and then eliminate EXACT duplicates just keeping the first instance of each
     return beers
-    
+
+
 def remove_dup_beer_rows(data, dup_params=("beer_name", "brewery_name")):
     """Remove rows where the review is of an inexact duplicate beer.
     By inexact, we mean they agree on only some subset of the parameters.
@@ -59,47 +69,55 @@ def remove_dup_beer_rows(data, dup_params=("beer_name", "brewery_name")):
         The columns where agreement determines duplicates. Defaults to
         "beer_name" and "brewery_name".
     """
-    beers = get_beers(data) # get all the beers
-    
+    beers = get_beers(data)  # get all the beers
+
     # Then extract all beer_beerids which occur as INEXACT duplicates.
-    inexact_dup_ids = beers[beers.duplicated(dup_params, keep = False)]['beer_beerid']
-    
+    inexact_dup_ids = beers[beers.duplicated(dup_params, keep=False)]["beer_beerid"]
+
     return data[~data.beer_beerid.isin(inexact_dup_ids)]
+
 
 def merge_similar_name_breweries(data):
     """Clean the reviews data by merging similar named breweries to have one common name.
     The list of breweries for which we do this is not expected to be exhaustive.
-    
+
     Parameters
     ----------
     data: pandas.DataFrame
         The Beer Advocate data frame.
     """
-    similar_names = [["BJ's Restaurant & Brewhouse",
-                      "BJ's Restaurant And Brewhouse",
-                      "BJ's Restaurant & Brewery"],
-                    ["Hops Grillhouse & Brewery",
-                     "Hops Grill & Brewery"],
-                    ["Rock Bottom Restaurant & Brewery",
-                     "Rock Bottom Restaurant and Brewery"],
-                    ["Les Trois Brasseurs",
-                     "Les 3 Brasseurs"]]
+    similar_names = [
+        [
+            "BJ's Restaurant & Brewhouse",
+            "BJ's Restaurant And Brewhouse",
+            "BJ's Restaurant & Brewery",
+        ],
+        ["Hops Grillhouse & Brewery", "Hops Grill & Brewery"],
+        ["Rock Bottom Restaurant & Brewery", "Rock Bottom Restaurant and Brewery"],
+        ["Les Trois Brasseurs", "Les 3 Brasseurs"],
+    ]
     for similar_name in similar_names:
-            data.loc[data.brewery_name.isin(similar_name[1:]),'brewery_name'] = similar_name[0]
+        data.loc[
+            data.brewery_name.isin(similar_name[1:]), "brewery_name"
+        ] = similar_name[0]
     return data
-    
+
+
 def merge_brewery_ids(data):
-    """ Assign a single brewery id to breweries that have the same name.
-    
+    """Assign a single brewery id to breweries that have the same name.
+
     Parameters
     ----------
     data: pandas.DataFrame
         The Beer Advocate data frame.
     """
-    breweries = data[['brewery_name','brewery_id']].drop_duplicates()
-    duplicates = breweries.brewery_name.value_counts().loc[breweries.brewery_name.value_counts()>1]
+    breweries = data[["brewery_name", "brewery_id"]].drop_duplicates()
+    duplicates = breweries.brewery_name.value_counts().loc[
+        breweries.brewery_name.value_counts() > 1
+    ]
     # go through the reviews and update the brewery_id to be the largest id corresponding to a given name
     for duplicate in duplicates.index:
-        data.loc[data.brewery_name==duplicate,'brewery_id'] = breweries.loc[breweries.brewery_name==duplicate,
-                                                                             'brewery_id'].max()
+        data.loc[data.brewery_name == duplicate, "brewery_id"] = breweries.loc[
+            breweries.brewery_name == duplicate, "brewery_id"
+        ].max()
     return data
--- train_test_split.py
+++ train_test_split.py
@@ -1,9 +1,10 @@
 """Methods to execute our validation scheme"""
 import numpy as np
 
+
 def get_splits(int_matrix, random_state=42):
     """Returns the matrix coordinates for 5 train and test splits.
-    
+
     Parameters
     ----------
     int_matrix: 2d matrix
@@ -21,30 +22,32 @@ def get_splits(int_matrix, random_state=42):
 
     N_users = int_matrix.shape[0]
     test_sets = [[] for _ in range(n_splits)]
-    train_set = [] # this set is never in the test set
+    train_set = []  # this set is never in the test set
     # for each user, separate their reviews into a train set and different test sets.
     for i in range(N_users):
         row_start = indptr[i]
         row_end = indptr[i + 1]
-        user_reviews = indices[row_start:row_end] # these are the beers that user i reviewed
+        user_reviews = indices[
+            row_start:row_end
+        ]  # these are the beers that user i reviewed
         n_reviews = len(user_reviews)
         # place the users in buckets based on number of reviews
         if n_reviews < 5:
             for beer_idx in user_reviews:
-                train_set.append([i,beer_idx])
+                train_set.append([i, beer_idx])
         elif n_reviews < 11:
             user_reviews = np.random.permutation(user_reviews)
             for j in range(n_splits):
-                test_sets[j].append([i,user_reviews[j]])
+                test_sets[j].append([i, user_reviews[j]])
             for beer_idx in user_reviews[n_splits:]:
-                train_set.append([i,beer_idx])
-        else :
+                train_set.append([i, beer_idx])
+        else:
             k = n_reviews // 10
             user_reviews = np.random.permutation(user_reviews)
             for j in range(n_splits):
-                for beer_idx in user_reviews[j*k:(j+1)*k]:
+                for beer_idx in user_reviews[j * k : (j + 1) * k]:
                     test_sets[j].append([i, beer_idx])
-            for beer_idx in user_reviews[n_splits*k:]:
+            for beer_idx in user_reviews[n_splits * k :]:
                 train_set.append([i, beer_idx])
     # create the splits by appending all but one test set to the train set
     splits = []
